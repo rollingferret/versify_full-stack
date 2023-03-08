d23a5bceac9528a346ae2d11bef40592826f534b
Irene Martinez <imartinez921@gmail.com>
2023-03-07 21:41:15 -0500

    Preliminary setup for UI reducers (playlistDropdown and playlistEditModal)

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,10 +4,12 @@ import usersReducer from './users_reducer';
 import playlistIndexReducer from './playlist_index_reducer';
 import currentPlaylistReducer from './current_playlist_reducer';
 
+
 const entitiesReducer = combineReducers({
     users: usersReducer,
     playlists: playlistIndexReducer,
     currentPlaylist: currentPlaylistReducer,
+
 })
 
 
