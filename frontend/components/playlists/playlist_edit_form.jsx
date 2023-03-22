2279761f144804dc8f32e6850e00ed3866cccdbc
Irene Martinez <imartinez921@gmail.com>
2023-03-22 15:07:48 -0400

    Create setState Hooks for user editing local state playlist title and description, using props as default state values. Create input tags (label, id, type, name) for title and description inputs, including onChange handler using Set Hooks

--- frontend/components/playlists/playlist_edit_form.jsx
+++ frontend/components/playlists/playlist_edit_form.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 // Consider writing as functional Component with React Hooks
 
-const PlaylistEditForm = ({
-    currentPlaylist,
-    editPlaylist,
-}) => {
+const PlaylistEditForm = ({ currentPlaylist, editPlaylist }) => {
     
     const playlistId = currentPlaylist.id;
     const userId = currentPlaylist.user_id;
-    const { title, description } = currentPlaylist;
+    const oldTitle = currentPlaylist.title;
+    const oldDescription = currentPlaylist.description;
+    
+    // Set our local slices of State
+    const [newTitle, setNewTitle] = useState(oldTitle);
+    const [newDescription, setNewDescription] = useState(oldDescription);
 
     console.log('User ID', userId)
+
+
+    // Submit button action
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('SUBMIT CLICKED')
+    }
 
     return <>
         <h3> This is PlaylistEditForm</h3>
         <div className="playlist-art-edit"></div>
+
         <form className="edit-content">
-            <input type="text" name="title" id="" />
-            <input type="text" name="description" id="" />
+            <label htmlFor="Playlist Title">
+                <input type="text" 
+                    id="Playlist Title" 
+                    name="title" 
+                    value={newTitle}
+                    onChange={e => setNewTitle(e.target.value)}
+                />
+            </label>
+            <br />
+            <label htmlFor="Playlist Description">
+                <input type="text" 
+                    id="Playlist Description" 
+                    name="description" 
+                    value={newDescription}
+                    onChange={e => setNewDescription(e.target.value)}
+                />
+            </label>
+            <br />
             <input type="submit" value="Save" />
         </form>
     </>
