5fa9e722358a48d15e5586581384feddafcbec73
Irene Martinez <imartinez921@gmail.com>
2023-04-04 16:00:01 -0400

    Set up Redux reducers for Artists + artistIndexReducer ~ Add artists to entitiesReducer ~ Modify name of playlistIndexReducer from playlistReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,14 +2,15 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './users_reducer';
 import playlistIndexReducer from './playlist_index_reducer';
+import artistIndexReducer from './artist_index_reducer';
 import currentPlaylistReducer from './current_playlist_reducer';
 
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     playlists: playlistIndexReducer,
+    artists: artistIndexReducer,
     currentPlaylist: currentPlaylistReducer,
-
 })
 
 
