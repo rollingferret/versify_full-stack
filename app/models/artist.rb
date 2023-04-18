a7ff6648fa579b2727eae0cc17b5036234f18210
Irene Martinez <imartinez921@gmail.com>
2023-04-18 16:51:11 -0400

    Modify Artists Controller so Artist#show will eager-load associated Albums ~ Artist Controller: Artist#show uses .includes to eager load :albums association + Artist Show Jbuilder view appending associated albums to Artist object

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,6 +6,7 @@
 #  name       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  photo_url  :string
 #
 class Artist < ApplicationRecord
     validates :name, presence:true
