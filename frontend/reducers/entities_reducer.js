ec0d2d9f3fed27e9808f012a4bf1c432ecc5c2cf
Irene Martinez <imartinez921@gmail.com>
2023-04-26 17:09:47 -0400

    Add an artist's associated albums to albums slice of state in Redux Store + Create albumIndexReducer to receive associated albums upon Artist#show + Import RECEIVE_CURRENT_ARTIST action to albumIndexReducer to access artist's associated albums ~ Add albumIndexReducer to entitiesReducer to set Albums slice of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import playlistIndexReducer from './playlist_index_reducer';
 import artistIndexReducer from './artist_index_reducer';
+import albumIndexReducer from './album_index_reducer';
 import currentItemReducer from './current_item_reducer';
 
 
@@ -10,6 +11,7 @@ const entitiesReducer = combineReducers({
     users: usersReducer,
     playlists: playlistIndexReducer,
     artists: artistIndexReducer,
+    albums: albumIndexReducer,
     currentItem: currentItemReducer,
 })
 
