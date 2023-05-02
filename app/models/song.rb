c7aeb1cf32d414faf4bdf01bc899f17aafceb091
Irene Martinez <imartinez921@gmail.com>
2023-05-02 18:23:09 -0400

    Defer collabartist associations for now

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,18 @@
 class Song < ApplicationRecord
-  belongs_to :album
-  belongs_to :collabartist1
-  belongs_to :collabartist2
-  belongs_to :collabartist3
+  belongs_to :album,
+  foreign_key: :album_id,
+  class_name: :Album
+
+  # belongs_to :collabartist1,
+  # foreign_key: :collabartist1_id,
+  # class_name: :Artist
+
+  # belongs_to :collabartist2,
+  # foreign_key: :collabartist2_id,
+  # class_name: :Artist
+
+  # belongs_to :collabartist3,
+  # foreign_key: :collabartist3_id,
+  # class_name: :Artist
+
 end
