6b00741c0cc676f59784eb65718a5818b465dcd9
Irene Martinez <imartinez921@gmail.com>
2023-05-10 18:27:06 -0400

    Create Songs slice of state when running Artist#show + songIndexReducer accepts songs payload from RECEIVE_CURRENT_ARTIST + Add songIndexReducer to entitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,6 +4,7 @@ import usersReducer from './users_reducer';
 import playlistIndexReducer from './playlist_index_reducer';
 import artistIndexReducer from './artist_index_reducer';
 import albumIndexReducer from './album_index_reducer';
+import songIndexReducer from './song_index_reducer';
 import currentItemReducer from './current_item_reducer';
 
 
@@ -12,6 +13,7 @@ const entitiesReducer = combineReducers({
     playlists: playlistIndexReducer,
     artists: artistIndexReducer,
     albums: albumIndexReducer,
+    songs: songIndexReducer,
     currentItem: currentItemReducer,
 })
 
