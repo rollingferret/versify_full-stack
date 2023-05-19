426bb34e533bfa1e09d131d6b4006a7850070b19
Irene Martinez <imartinez921@gmail.com>
2023-05-18 21:43:50 -0400

    Add AlbumShowContainer to CurrentViewComponent

--- frontend/components/main/current_view.jsx
+++ frontend/components/main/current_view.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import HomeContainer from '../home/home_container.jsx'
 import PlaylistShowContainer from "../playlists/playlist_show_container";
 import ArtistShowContainer from "../artists/artist_show_container";
+import AlbumShowContainer from "../albums/album_show_container.js";
 
 const CurrentView = (props) => {
 
@@ -24,6 +25,9 @@ const CurrentView = (props) => {
                     params={params} currentUser={currentUser} history={history}/>;
             case "/artist/:id":
                 return <ArtistShowContainer
+                    params={params} currentUser={currentUser} history={history}/>;
+            case "/album/:id":
+                return <AlbumShowContainer
                     params={params} currentUser={currentUser} history={history}/>;
             default:
                 return <HomeContainer 
