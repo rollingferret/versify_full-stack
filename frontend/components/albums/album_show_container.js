1c378b6a3d229b55fb3a78928449789bd7bd57b2
Irene Martinez <imartinez921@gmail.com>
2023-05-18 21:48:18 -0400

    Create AlbumShowContainer + Import displayAlbum and clearCurrent actions + mapStateToProps pulls current album, tracks, and album artist after querying database with ID from the URL params

---
+++ frontend/components/albums/album_show_container.js
@@ -0,0 +1,25 @@
+import { connect } from "react-redux";
+import { clearCurrent,
+} from "../../actions/playlists_actions";
+import { displayAlbum,
+} from "../../actions/album_actions";
+
+import AlbumShow from "./album_show";
+
+const mapStateToProps = (state, ownProps) => {
+
+    return {
+    currentAlbum: state.entities.currentItem,
+    tracks: state.entities.songs,
+    albumArtist: state.entities.currentItem.tracks,
+    params: ownProps.params,
+    currentUser: ownProps.currentUser,
+    history: ownProps.history,
+}}
+
+const mapDispatchToProps = dispatch => ({
+    displayAlbum: (albumId) => dispatch( displayAlbum(albumId) ),
+    clearCurrent: () => dispatch( clearCurrent() ),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumShow);
