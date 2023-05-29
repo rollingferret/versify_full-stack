6ad5492ff393cdd9e61fb6704adfb5025367fccb
Irene Martinez <imartinez921@gmail.com>
2023-05-29 15:41:33 -0400

    Modify migration for CreatePlaylistedSong

--- db/migrate/20230529192330_create_playlisted_songs.rb
+++ db/migrate/20230529192330_create_playlisted_songs.rb
@@ -1,12 +1,13 @@
 class CreatePlaylistedSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :playlisted_songs do |t|
-      t.integer :song_id
-      t.integer :playlist_id
+      t.integer :song_id, null: false
+      t.integer :playlist_id, null: false
       t.string :timestamps
 
       t.timestamps
     end
+
     add_index :playlisted_songs, :song_id
     add_index :playlisted_songs, :playlist_id
   end
