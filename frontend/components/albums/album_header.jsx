1b781bcb92755f8f06d880735df649499f5f86a4
Irene Martinez <imartinez921@gmail.com>
2023-05-29 10:01:24 -0400

    Clean up Album Header + Add albumArtist ArtistLink to AlbumHeader ~ Only show hours if hrs props is not zero

--- frontend/components/albums/album_header.jsx
+++ frontend/components/albums/album_header.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 
+import ArtistLink from '../artists/artist_link';
+
 const AlbumHeader = ({ album,
+  history,
 }) => {
 
   const { id,
       title,
       genre,
       year,
+      albumArtist,
       albumPhotoUrl,
       number,
       hrs,
       mins,
-      duration,
   } = album;
 
   const songWord = number === 1 ? "song" : "songs";
 
-  return (
-    <>
-      <div className="album-art">
-        <img src={albumPhotoUrl} />
+  const hours = hrs === 0 ? "" : `${hrs} h `;
+
+  const header = (<>
+    <div className="album-art">
+      <img src={albumPhotoUrl} />
+    </div>
+    <div className="album-header-text">
+      <h2>Album</h2>
+      <div className="album-title">
+        {title}
       </div>
-      <div className="album-header-text">
-        <h2>Album</h2>
-        <div className="playlist-title">
-          {title}
-        </div>
-        <div id="description">
-          <h3>{genre} • {year} • {number} {songWord}, {hrs} h {mins} min</h3>
-        </div>
+      <div className="album-description">
+        <h3>
+          <ArtistLink artist={albumArtist} currentArtist={null} 
+            history={history}/>
+          &nbsp;• {genre} • {year} • {number} {songWord}, {hours} {mins} min
+        </h3>
       </div>
-    </>
-  );
+    </div>
+  </>)
+
+  return albumArtist ? header : null   
+  // ArtistLink wil error out immediately if albumArtist prop is not yet passed
 };
 
 export default AlbumHeader;
