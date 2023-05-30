df18b1faee15cd622171391a798cef95d804f4cc
Irene Martinez <imartinez921@gmail.com>
2023-05-30 13:34:02 -0400

    Create initial #create, #destroy, #index methods for PlaylistedSongs

--- app/controllers/api/playlisted_song_controller.rb
+++ app/controllers/api/playlisted_song_controller.rb
@@ -1,2 +1,37 @@
 class Api::PlaylistedSongController < ApplicationController
+
+    def create
+        @playlisted_song = PlaylistedSong.new(playlisted_song_params)
+        if @playlisted_song.save!
+            render :show
+        else
+            render json: @playlisted_song.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @playlisted_song = PlaylistedSong.find(params[:id])
+        if @playlisted_song && (@playlisted_song.playlist.user_id == current_user.id)
+            if @playlisted_song.destroy!
+                render :show
+            end
+        else
+            render json: @playlisted_song.errors.full_messages, status: 422
+        end
+    end
+
+
+    def index
+        @playlist_songs = PlaylistedSong.includes(:song).find_by(playlist_id: params[:playlist_id])
+        render :index
+    end
+
+
+
+
+
+    private
+    def playlisted_song_params
+        params.require(:playlisted_song).permit(:song_id, :playlist_id)
+    end
 end
