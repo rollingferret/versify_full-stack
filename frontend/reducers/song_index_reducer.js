3ef58c8f125dde77ae151dc2c03ecd1714e3e4cc
Irene Martinez <imartinez921@gmail.com>
2023-05-30 20:23:58 -0400

    Modify SongIndexReducer to take songs payload instead of checking songs array length first (zero songs will result in an empty array anyway)

--- frontend/reducers/song_index_reducer.js
+++ frontend/reducers/song_index_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CURRENT_PLAYLIST,
+    RESET_CURRENT,
 } from "../actions/playlist_actions";
 import { RECEIVE_CURRENT_ARTIST,
 } from "../actions/artist_actions";
@@ -10,7 +11,7 @@ const songIndexReducer = (songsState = {}, action) => {
     Object.freeze(songsState);
     switch (action.type) {
         case RECEIVE_CURRENT_PLAYLIST:
-            return action.songs.length > 0 ? action.songs : songsState;
+            return action.songs;
         case RECEIVE_CURRENT_ARTIST:
             return action.songs;
         case RECEIVE_CURRENT_ALBUM:
