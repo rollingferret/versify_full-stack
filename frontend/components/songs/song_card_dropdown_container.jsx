0b3a8d273f0c188b9b48d723f9528ce465f24bd2
Irene Martinez <imartinez921@gmail.com>
2023-07-03 18:44:03 -0400

    Add displayPlaylist, currentItem, and playlists slice of state to SongCardDropdownContainer

--- frontend/components/songs/song_card_dropdown_container.jsx
+++ frontend/components/songs/song_card_dropdown_container.jsx
@@ -3,11 +3,15 @@ import { connect } from "react-redux";
 import { removePlaylisted,
     createNewPlaylisted,
 } from "../../actions/playlisted_actions";
+import { displayPlaylist,
+} from "../../actions/playlist_actions";
 
 import SongCardDropdown from "./song_card_dropdown";
 
 
 const mapStateToProps = (state, ownProps) => ({
+    currentItem: state.entities.currentItem,
+    playlists: state.entities.playlists,
     source: ownProps.source,
     selectedSong: ownProps.selectedSong,
     history: ownProps.history,
@@ -15,13 +19,12 @@ const mapStateToProps = (state, ownProps) => ({
     updateSongCardDropdownState: ownProps.updateSongCardDropdownState,
     items: ownProps.items, // already contains current playlists from SongIndex
     depthLevel: ownProps.depthLevel,
-    updateSelectedIndex1: ownProps.updateSelectedIndex1,
-    updateSelectedIndex2: ownProps.updateSelectedIndex2,
 })
 
 const mapDispatchToProps = (dispatch) => ({
     removePlaylisted: (playlistedId) => dispatch( removePlaylisted(playlistedId) ),
     createNewPlaylisted: ( songId, playlistId) => dispatch( createNewPlaylisted(songId, playlistId) ),
+    displayPlaylist: (playlistId) => dispatch( displayPlaylist(playlistId) ),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SongCardDropdown);
