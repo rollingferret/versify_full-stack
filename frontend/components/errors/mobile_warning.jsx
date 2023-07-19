854a931d8bedb5ec54b38b5b28c815afedcc394f
Irene Martinez <imartinez921@gmail.com>
2023-07-18 21:26:46 -0400

    Add message to block from mobile view

---
+++ frontend/components/errors/mobile_warning.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MobileWarning = () => {
+
+    return (
+        <div className="mobile-overlay">
+            <div className="mobile-message">
+                Currently this app is for desktop use only. 
+                <br/><br/>
+                We sincerely apologize for any
+                inconvenience.
+            </div>
+        </div>
+    );
+};
+
+export default MobileWarning;
