3fcbc6faa96c1ad9a746d985d7430141cba323b0
Irene Martinez <imartinez921@gmail.com>
2023-07-18 21:26:46 -0400

    Add message to block from mobile view

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 import NavBarSplashContainer from "../nav_bar/nav_bar_splash_container";
 import SplashDisplay from "./splash_display";
+import MobileWarning from "../errors/mobile_warning";
 
 const Splash = (props) => {
     const { currentUser, history } = props;
  
     return (
-        <div className="splash-page">
-            <NavBarSplashContainer history={history}/>
-            <section className='splash-display'>
-                    <SplashDisplay currentUser={currentUser} history={history}/>
-            </section>
-        </div>
+        <>
+            <MobileWarning />
+            <div className="splash-page">
+                <NavBarSplashContainer history={history}/>
+                <section className='splash-display'>
+                        <SplashDisplay currentUser={currentUser} history={history}/>
+                </section>
+            </div>
+        </>
     )
 
 }
