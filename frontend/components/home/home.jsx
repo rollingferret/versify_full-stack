46519c8bcfcabea3881051f639f411ea77b28e5e
Irene D. Martinez <imartinez921@gmail.com>
2023-07-26 18:23:31 -0400

    [Page,Home] Add testing view widths

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,16 +3,16 @@ import React from "react";
 import ArtistIndexContainer from "../artists/artist_index_container";
 
 const Home = ({ params, history, path }) => {
-    return (
-        <div className="home">
-            <div className="home-header">Your Versify</div>
-            <ArtistIndexContainer
-                params={params}
-                history={history}
-                path={path}
-            />
-        </div>
-    );
+	return (
+		<div className="home">
+			<div className="home-header">Your Versify</div>
+			<ArtistIndexContainer
+				params={params}
+				history={history}
+				path={path}
+			/>
+		</div>
+	);
 };
 
 export default Home;
