9eb7860784a1a0d7c3d78bccede1efee64bb3d67
Irene D. Martinez <imartinez921@gmail.com>
2023-08-02 19:14:47 -0400

    [ArtistShow] Clarify formatting via Prettier

--- frontend/components/artists/artist_show.jsx
+++ frontend/components/artists/artist_show.jsx
@@ -4,67 +4,65 @@ import ArtistHeader from "./artist_header";
 import AlbumIndex from "../albums/album_index";
 import CollabSongIndex from "../songs/collab_song_index";
 
-const ArtistShow = (
-    {   currentArtist,
-        albums,
-        collabSongs,
-        params,
-        currentUser,
-        history,
-        path,
-        displayArtist,
-        displayAlbum,
-        clearCurrent,
-    }
-) => {
-    
-    useEffect( () => {
-        displayArtist(params.id);
+const ArtistShow = ({
+	currentArtist,
+	albums,
+	collabSongs,
+	params,
+	currentUser,
+	history,
+	path,
+	displayArtist,
+	displayAlbum,
+	clearCurrent,
+}) => {
+	useEffect(() => {
+		displayArtist(params.id);
 
-        const rendered = document.getElementById("artist-show");
-        rendered ? rendered.scrollTo(0, 0) : null;
+		const rendered = document.getElementById("artist-show");
+		rendered ? rendered.scrollTo(0, 0) : null;
 
-        return () => {
-            clearCurrent();
-        }
-    }, [params]); // Will run whenever params.id changes, otherwise ArtistShow doesn't re-render
+		return () => {
+			clearCurrent();
+		};
+	}, [params]); // Will run whenever params.id changes, otherwise ArtistShow doesn't re-render
 
-    const artistShow = (
+	const artistShow = (
 		<>
 			<div
 				className="artist-show-background"
 				style={{
 					backgroundImage: `url(${currentArtist.photoUrl})`,
 					backgroundSize: "50%",
-                    height: "100%",
+					height: "100%",
 				}}
 			>
-            <div className="artist-show" id="artist-show">
-                <div className="artist-header">
-                    <ArtistHeader currentArtist={currentArtist} />
-                </div>
-                <div className="artist-nav"></div>
-                {albums.length > 0 ? (
-                    <AlbumIndex
-                        albums={albums}
-                        history={history}
-                        displayAlbum={displayAlbum}
-                    />
-                ) : null}
-                {collabSongs.length > 0 ? (
-                    <CollabSongIndex
-                        songs={collabSongs}
-                        history={history}
-                        displayAlbum={displayAlbum}
-                        currentArtist={currentArtist}
-                    />
-                ) : null}
-            </div>
+				<div className="artist-show" id="artist-show">
+					<div className="artist-header">
+						<ArtistHeader currentArtist={currentArtist} />
+					</div>
+					<div className="artist-nav"></div>
+					{albums.length > 0 ? (
+						<AlbumIndex
+							albums={albums}
+							history={history}
+							displayAlbum={displayAlbum}
+						/>
+					) : null}
+					{collabSongs.length > 0 ? (
+						<CollabSongIndex
+							songs={collabSongs}
+							history={history}
+							displayAlbum={displayAlbum}
+							currentArtist={currentArtist}
+						/>
+					) : null}
+				</div>
 			</div>
 		</>
 	);
 
-    return currentArtist.photoUrl ? artistShow : null
-}
+	return currentArtist.photoUrl ? artistShow : null;
+};
 
 export default ArtistShow;
