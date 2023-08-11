81e74167dfb0ff9086a1849a4cc200edd698d74a
Irene D. Martinez <imartinez921@gmail.com>
2023-08-11 15:14:26 -0400

    [Player] Initialize Player states, refs, and functions)

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -2,25 +2,28 @@ import React, { useEffect, useRef, useState } from "react";
 import NowPlayingInfo from "./now_playing_info";
 import PlayingControls from "./playing_controls";
 
-
-const Player = ( {tracks} ) => {
-
-	const { title,
-		albumId,
-		audioUrl,
-		albumImageUrl,
-		songartist }
-	= tracks[trackIndex];
+const Player = ({ tracks }) => {
+	const { title, albumId, audioUrl, albumImageUrl, songArtist } =
+		tracks[trackIndex];
 
 	// Set local states
-	const {trackIndex, setTrackIndex} = useState(0);
-	const {trackProgress, setTrackProgress} = useState(0);
-	const {isPlaying, setIsPlaying} = useState(false);
+	const { trackIndex, setTrackIndex } = useState(0);
+	const { trackProgress, setTrackProgress } = useState(0);
+	const { isPlaying, setIsPlaying } = useState(false);
 
 	// Set refs
 	const audioRef = useRef(new Audio(audioUrl));
 	const intervalRef = useRef();
 	const isReady = useRef(false);
+
+	// Player functions
+	const toPrevTrack = () => {
+		console.log("TODO go to prev");
+	};
+
+	const toNextTrack = () => {
+		console.log("TODO go to next");
+	};
 
 	return (
 		<div className="player-container">
