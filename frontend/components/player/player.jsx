439c256053f8a6d5145941200658afc728ac2626
Irene D. Martinez <imartinez921@gmail.com>
2023-08-14 13:46:27 -0400

    [Player] Create refs and PlayingControls functions

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -5,19 +5,62 @@ import PlayingControls from "./playing_controls";
 const Player = ({ tracks }) => {
 	const { title, albumId, audioUrl, albumImageUrl, songArtist } =
 		tracks[trackIndex];
-
+	
 	// Set local states
 	const { trackIndex, setTrackIndex } = useState(0);
-	const { trackProgress, setTrackProgress } = useState(0);
 	const { isPlaying, setIsPlaying } = useState(false);
+	const { trackProgress, setTrackProgress } = useState(0);
 
-	// Updater functions
-	const togglePlay = () => setIsPlaying(!isPlaying);
-
-	// Set refs
-	const audioRef = useRef(new Audio(audioUrl));
 	const intervalRef = useRef();
-	const isReady = useRef(false);
+
+	// Set up play/pause behavior
+	const audioRef = useRef(new Audio(audioUrl)); // creates a new HTMLAudioElement
+	useEffect(() => {
+		if (isPlaying) {
+			audioRef.current.play();
+		} else {
+			audioRef.current.pause();
+		}
+	}, [isPlaying]);
+
+	// Set up behavior when changing tracks
+	const isReady = useRef(false); // Avoids auto-play
+	useEffect(() => {
+		audioRef.current.pause();
+		audioRef.current.source = new Audio(audioUrl);
+		setTrackProgress(audioRef.current.currentTime);
+
+		if (isReady.current) {
+			audioRef.current.play();
+			setIsPlaying(true);
+			startTimer();
+		} else {
+			// Set the isReady ref as true for the next render
+			isReady.current = true;
+		}
+	}, [trackIndex]);
+
+	// Create PlayingControls functions
+	const togglePlay = () => setIsPlaying(!isPlaying);
+	const toPrevTrack = () => {
+		if (trackIndex - 1 < 0) {
+			setTrackIndex(tracks.length - 1);
+		} else {
+			setTrackIndex(trackIndex - 1);
+		}
+	};
+	const toNextTrack = () => {
+		if (trackIndex < tracks.length - 1) {
+			setTrackIndex(trackIndex + 1);
+		} else {
+			setTrackIndex(0);
+		}
+	};
+	const shufflePlay = () => {
+		if (tracks.length > 1) {
+			setTrackIndex(Math.floor(Math.random() * tracks.length));
+		}
+	};
 
 	return (
 		<div className="player-container">
@@ -25,6 +68,9 @@ const Player = ({ tracks }) => {
 			<PlayingControls
 				isPlaying={isPlaying}
 				togglePlay={togglePlay}
+				toPrevTrack={toPrevTrack}
+				toNextTrack={toNextTrack}
+				shufflePlay={shufflePlay}
 			/>
 			<div className="player-right"></div>
 		</div>
