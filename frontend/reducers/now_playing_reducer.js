04d5f39ae2f27a0e6cfb1af47ee423198a187d61
Irene D. Martinez <imartinez921@gmail.com>
2023-08-16 15:59:29 -0400

    Add allSongs from ArtistShow to nowPlaying queue

--- frontend/reducers/now_playing_reducer.js
+++ frontend/reducers/now_playing_reducer.js
@@ -1,10 +1,21 @@
-import { PUSH_PLAY,
-    PUSH_PAUSE,
+import { RECEIVE_CURRENT_ARTIST } from "../actions/artist_actions";
+import { PLAY_ARTIST,
+    PLAY_ALBUM,
+    PLAY_PLAYLIST,
 } from "../actions/now_playing_actions";
 
-const nowPlayingReducer = (playState={}, action) => {
+const nowPlayingReducer = (playState={isPlaying: false, trackIndex: 0, tracks:[], isShuffling: false}, action) => {
     Object.freeze(playState);
+    let newPlayState = Object.assign({}, playState)
     switch (action.type) {
+        case RECEIVE_CURRENT_ARTIST:
+            newPlayState.tracks = [...action.allSongs];
+            return newPlayState;
+		// case RECEIVE_CURRENT_PLAYLIST:
+        //     newPlayState.tracks = action.songs;
+		// 	return newPlayState;
+		// case PLAY_PLAYLIST:
+        //     newPlayState.isPlaying = true;
 		default:
 			return playState;
 	}
