550b8322bda3a00228799b49b18f9dde6b774b3a
Irene D. Martinez <imartinez921@gmail.com>
2023-08-16 16:49:48 -0400

    Initiate and test NowPlaying Redux actions

--- frontend/reducers/now_playing_reducer.js
+++ frontend/reducers/now_playing_reducer.js
@@ -1,18 +1,23 @@
 import { RECEIVE_CURRENT_ARTIST } from "../actions/artist_actions";
-import { PLAY_ARTIST,
+import { TOGGLE_PLAY,
+    PLAY_ARTIST,
     PLAY_ALBUM,
     PLAY_PLAYLIST,
 } from "../actions/now_playing_actions";
 
-const nowPlayingReducer = (playState={isPlaying: false, trackIndex: 0, tracks:[], isShuffling: false}, action) => {
+const nowPlayingReducer = (playState={isPlaying: false, trackIndex: 0, queue:[], isShuffling: false}, action) => {
     Object.freeze(playState);
     let newPlayState = Object.assign({}, playState)
     switch (action.type) {
         case RECEIVE_CURRENT_ARTIST:
-            newPlayState.tracks = [...action.allSongs];
+            newPlayState.queue = [...action.allSongs];
+            return newPlayState;
+        case TOGGLE_PLAY:
+            debugger
+            if (newPlayState.queue?.length > 0) newPlayState.isPlaying = !newPlayState.isPlaying;
             return newPlayState;
 		// case RECEIVE_CURRENT_PLAYLIST:
-        //     newPlayState.tracks = action.songs;
+        //     newPlayState.queue = action.songs;
 		// 	return newPlayState;
 		// case PLAY_PLAYLIST:
         //     newPlayState.isPlaying = true;
