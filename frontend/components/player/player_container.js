2a90ed2db54c9e5417e4541ef7564d7dd95d7717
Irene D. Martinez <imartinez921@gmail.com>
2023-08-18 18:07:38 -0400

    Add queue to Player Container from Redux

--- frontend/components/player/player_container.js
+++ frontend/components/player/player_container.js
@@ -1,20 +1,26 @@
 import { connect } from "react-redux";
-import Player from "./footer";
+import Player from "./player";
+
+import { reduxPlay,
+} from "../../actions/now_playing_actions";
 
 const mapStateToProps = (
-	{ session, entities: { users }, errors },
+	// { session, entities: { users, nowPlaying }, errors },
+	state,
 	ownProps
-) => ({
-	currentUser: users[session.id],
-	errors: errors,
-	// matchObj is a prop passed down by AuthRoute. matchObj = {params, path, url} as keys
-	params: ownProps.match.params,
-	path: ownProps.match.path,
-	history: ownProps.history,
-});
+) => {
+	return {
+		currentUser: state.entities.users[state.session.id],
+		errors: state.entities.errors,
+		params: ownProps.params,
+		path: ownProps.path,
+		history: ownProps.history,
+		tracks: state.entities.nowPlaying.queue,
+	};};
 
 const mapDispatchToProps = (dispatch) => ({
 	clearPlaylistErrors: () => dispatch(clearPlaylistErrors()),
+	reduxPlay: () => dispatch(reduxPlay()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Player);
