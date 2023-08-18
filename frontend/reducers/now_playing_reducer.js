437ab5f3aaadf95c0237a2041725acf88bcf877e
Irene D. Martinez <imartinez921@gmail.com>
2023-08-18 18:32:00 -0400

    [nowPlayingReducer] Use Redux for play/queue * Queue allSongs from current view (eg. ArtistShow, PlaylistShow)

--- frontend/reducers/now_playing_reducer.js
+++ frontend/reducers/now_playing_reducer.js
@@ -1,29 +1,23 @@
 import { RECEIVE_CURRENT_ARTIST } from "../actions/artist_actions";
-import { TOGGLE_PLAY,
-    PLAY_ARTIST,
-    PLAY_ALBUM,
-    PLAY_PLAYLIST,
-} from "../actions/now_playing_actions";
+import { TOGGLE_PLAY } from "../actions/now_playing_actions";
 
-const nowPlayingReducer = (playState={isPlaying: false, trackIndex: 0, queue:[], isShuffling: false}, action) => {
-    Object.freeze(playState);
-    let newPlayState = Object.assign({}, playState)
-    switch (action.type) {
-        case RECEIVE_CURRENT_ARTIST:
-            newPlayState.queue = [...action.allSongs];
-            return newPlayState;
-        case TOGGLE_PLAY:
-            debugger
-            if (newPlayState.queue?.length > 0) newPlayState.isPlaying = !newPlayState.isPlaying;
-            return newPlayState;
-		// case RECEIVE_CURRENT_PLAYLIST:
-        //     newPlayState.queue = action.songs;
-		// 	return newPlayState;
-		// case PLAY_PLAYLIST:
-        //     newPlayState.isPlaying = true;
+const nowPlayingReducer = (
+	playState = { isPlaying: false, queue: [] },
+	action
+) => {
+	Object.freeze(playState);
+	let newPlayState = Object.assign({}, playState);
+	switch (action.type) {
+		case RECEIVE_CURRENT_ARTIST:
+			newPlayState.queue = [...action.allSongs];
+			return newPlayState;
+		case TOGGLE_PLAY:
+			if (newPlayState.queue?.length > 0)
+				newPlayState.isPlaying = !newPlayState.isPlaying;
+			return newPlayState;
 		default:
 			return playState;
 	}
-}
+};
 
 export default nowPlayingReducer;
