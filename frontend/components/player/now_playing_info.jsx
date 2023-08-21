78637e3cee5056bf5208a691f31575cb30456d3a
Irene D. Martinez <imartinez921@gmail.com>
2023-08-20 20:34:02 -0400

    [NowPlayingInfo] Give default track prop

--- frontend/components/player/now_playing_info.jsx
+++ frontend/components/player/now_playing_info.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-const NowPlayingInfo = (track) => {
-	const { title, albumId, audioUrl, albumImageUrl, songArtist } =
-		track;
+const NowPlayingInfo = (
+	track = {
+		title: "",
+		albumId: "",
+		audioUrl: "",
+		albumImageUrl: "",
+		songArtist: "",
+	}
+) => {
+	const { title, albumId, audioUrl, albumImageUrl, songArtist } = track;
 	return (
 		<div className="now-playing-info">
 			<div
 				className="now-playing-art"
-				alt={`track artwork for ${title} by ${songArtist}`}
-			></div>
+				alt={`track artwork for ${track.title} by ${track.songArtist}`}
+			>
+				{track.title}
+			</div>
 			<div className="now-playing-info">
 				<div className="now-playing-title">{title}</div>
 				<div className="now-playing-artist">{songArtist}</div>
