e1344ad3a9d5343768a85b47dc7f5d14c5dc20d4
Irene D. Martinez <imartinez921@gmail.com>
2023-08-24 18:36:53 -0400

    [NowPlayingInfo] Add default track; finetune info

--- frontend/components/player/now_playing_info.jsx
+++ frontend/components/player/now_playing_info.jsx
@@ -1,29 +1,44 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const NowPlayingInfo = ({
-	track = {
-		title: "",
-		albumId: "",
-		audioUrl: "",
-		albumImageUrl: "",
-		songArtist: "",
-	},
-	...restProps
+	audioRef,
+	track,
+	trackProgress,
+	isPlaying,
+	updateTrackProgress,
 }) => {
+	if (!track) {
+		track = {
+			title: "",
+			albumId: "",
+			audioUrl: "",
+			albumImageUrl: "",
+			songArtist: "",
+		};
+	}
 	const { title, albumId, audioUrl, albumImageUrl, songArtist } = track;
+	useEffect(() => {
+		updateTrackProgress(audioRef.current.currentTime);
+	}, [isPlaying]);
 	return (
 		<div className="now-playing">
-			<div
-				className="now-playing-art"
-				alt={`track artwork for ${title} by ${songArtist.name}`}
-			>
-				<img src={albumImageUrl} />
-			</div>
-			<div className="now-playing-info">
-				<div className="now-playing-title">{title}</div>
-				<div className="now-playing-artist">{songArtist.name}</div>
-			</div>
-			<div className="now-playing-buttons"></div>
+			{trackProgress > 0 || (isPlaying && trackProgress === 0) ? (
+				<>
+					<div
+						className="now-playing-art"
+						alt={`track artwork for ${title} by ${songArtist.name}`}
+					>
+						{albumImageUrl ? <img src={albumImageUrl} /> : null}
+					</div>
+					<div className="now-playing-info">
+						<div className="now-playing-title">{title}</div>
+						<div className="now-playing-artist">
+							{songArtist.name}
+						</div>
+					</div>
+					<div className="now-playing-buttons"></div>
+				</>
+			) : null}
 		</div>
 	);
 };
