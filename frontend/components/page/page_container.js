6b4306cd49ffab6a776a6adbac6f43b45637cd68
Irene D. Martinez <imartinez921@gmail.com>
2023-08-26 15:57:36 -0400

    [PageContainer] Cleanup comment

--- frontend/components/page/page_container.js
+++ frontend/components/page/page_container.js
@@ -6,7 +6,6 @@ const mapStateToProps = (state, ownProps) => ({
 	currentUser: state.entities.users[state.session.id],
 	errors: state.entities.errors,
 	// matchObj is a prop passed down by AuthRoute
-	// matchObj = {params, path, url} as keys
 	params: ownProps.match.params,
 	path: ownProps.match.path,
 	history: ownProps.history,
