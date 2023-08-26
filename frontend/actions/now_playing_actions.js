8c798ca5942c3132bf6887d5864f563e730465eb
Irene D. Martinez <imartinez921@gmail.com>
2023-08-26 15:53:46 -0400

    Remove Shuffle Redux actions for now

--- frontend/actions/now_playing_actions.js
+++ frontend/actions/now_playing_actions.js
@@ -1,9 +1,6 @@
 export const TOGGLE_PLAY = 'TOGGLE_PLAY'
 export const PLAY_ALBUM = 'PLAY_ALBUM'
 export const PLAY_PLAYLIST = 'PLAY_PLAYLIST'
-export const SHUFFLE_ARTIST = 'SHUFFLE_ARTIST'
-export const SHUFFLE_ALBUM = 'SHUFFLE_ALBUM'
-export const SHUFFLE_PLAYLIST = 'SHUFFLE_PLAYLIST'
 
 const togglePlay = () => ({
     type: TOGGLE_PLAY,
