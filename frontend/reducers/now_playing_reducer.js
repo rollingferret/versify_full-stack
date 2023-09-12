8b8bcc6f4e83330f938e442031baba41ecbcccdc
Irene D. Martinez <imartinez921@gmail.com>
2023-09-12 17:15:57 -0400

    Initialize QUEUE_ALBUM Redux action

--- frontend/reducers/now_playing_reducer.js
+++ frontend/reducers/now_playing_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENT_ARTIST } from "../actions/artist_actions";
+import { QUEUE_ALBUM } from "../actions/now_playing_actions";
 import { TOGGLE_PLAY } from "../actions/now_playing_actions";
 
 const nowPlayingReducer = (
@@ -8,13 +8,15 @@ const nowPlayingReducer = (
 	Object.freeze(playState);
 	let newPlayState = Object.assign({}, playState);
 	switch (action.type) {
-		case RECEIVE_CURRENT_ARTIST:
-			newPlayState.queue = [...action.allSongs];
-			return newPlayState;
 		case TOGGLE_PLAY:
 			if (newPlayState.queue?.length > 0)
 				newPlayState.isPlaying = !newPlayState.isPlaying;
 			return newPlayState;
+		case QUEUE_ALBUM: // payload is an array of song objects
+			action.songs.forEach((song) => {
+				newPlayState.queue.push(song);
+				console.log("newPlayState.queue", newPlayState.queue)
+			});
 		default:
 			return playState;
 	}
